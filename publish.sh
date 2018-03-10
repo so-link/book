16b77034a616abef7f9bc9f2eb970aa64df5b175
钟嘉杰 <sunwback@qq.com>
2018-03-10 12:37:30 +0000

    更新 'publish.sh'

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 cd /home/gitbook/book
 git pull
 docker run  -v /home/gitbook/book:/book -v /home/gitbook/blog:/blog node-gitbook gitbook install
-docker run -v /home/gitbook/book:/book -v /home/gitbook/blog:/blog node-gitbook gitbook build /book /blog
+docker run -v /home/gitbook/book:/book -v /home/gitbook/blog:/blog node-gitbook gitbook build /book /blog > build_blog.log
