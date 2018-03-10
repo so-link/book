4f1d745cfa831b7296fc208569a0d9dcacdd7f01
cweihang <cweihang@foxmail.com>
2018-03-10 16:00:23 +0800

    remove some plugins

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
-gitbook install
-gitbook build . ../tmp_blog
+sudo docker run  -v /home/cwh/book:/book -v /home/cwh/blog:/blog node-gitbook gitbook install
+sudo docker run  -v /home/cwh/book:/book -v /home/cwh/blog:/blog node-gitbook gitbook build /book /blog
