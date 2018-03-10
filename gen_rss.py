aa5e68972e88a19c4d36bca85cddf17f94d8fb53
sunwback <sunwback@qq.com>
2018-03-10 23:31:43 +0800

    脚本添加生成rss

--- gen_rss.py
+++ gen_rss.py
@@ -1,6 +1,8 @@
 import re
 import os
 import time
+import datetime
+import PyRSS2Gen
 _re_link = re.compile(r'\[(.*)\]\((.*)\)$')
 
 BOOK_DIR = '/home/gitbook/book'
@@ -8,6 +10,7 @@ ROOT_URL = 'https://blog.so-link.org'
 
 SUMMARY_MD_PATH = os.path.join(BOOK_DIR, 'SUMMARY.md')
 POST_LIST_FILE_PATH = os.path.join(BOOK_DIR, 'posts/README.md')
+RSS_XML_PATH = os.path.join(BOOK_DIR, 'rss.xml')
 posts = []
 
 with open(SUMMARY_MD_PATH) as file:
@@ -30,4 +33,24 @@ posts.sort(key=lambda entry: (entry[2], entry[0]), reverse=True)
 with open(POST_LIST_FILE_PATH, 'w') as file:
     for title, path, mtime in posts:
         file.write('- [{} {}](/{}.html)\n\n'.format(time.strftime('%Y-%m-%d', mtime), title, path[:-3]))
+
+rss2items = []
+
+for title, path, mtime in posts:
+    rss2items.append(PyRSS2Gen.RSSItem(
+        title = title,
+        link = '{}/{}.html'.format(ROOT_URL, path[:-3]),
+        guid = PyRSS2Gen.Guid(path),
+        pubDate = datetime.datetime.fromtimestamp(time.mktime(mtime)),
+    ))
+
+rss = PyRSS2Gen.RSS2(
+    title = 'blog.so-link.org',
+    link = 'https://blog.so-link.org/',
+    items = rss2items,
+    description = 'solink blog',
+    lastBuildDate = datetime.datetime.now(),
+)
+
+rss.write_xml(open(RSS_XML_PATH, 'w'))
 
