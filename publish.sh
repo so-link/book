d626b0a84fa2cbc26ef368a9bca97e8fc03b4a09
陈伟航 <cweihang@foxmail.com>
2018-03-10 08:08:12 +0000

    更新 'publish.sh'

--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
+cd /home/gitbook/book
+git pull
 docker run  -v /home/gitbook/book:/book -v /home/gitbook/blog:/blog node-gitbook gitbook install
 docker run -v /home/gitbook/book:/book -v /home/gitbook/blog:/blog node-gitbook gitbook build /book /blog
